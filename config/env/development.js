d1437fa95fe98343446ac6f24e78bc5cc5ae2551
Rodrigo Januario da Silva <rjdsilv@gmail.com>
2019-03-22 16:34:08 -0400

    Created the basic application structure.

---
+++ config/env/development.js
@@ -0,0 +1,5 @@
+﻿// Set the 'development' environment configuration object
+module.exports = {
+    db: 'mongodb://localhost/lab03',
+    sessionSecret: 'developmentSessionSecret',
+};
---
+++ public/app/app.component.ts
@@ -0,0 +1,8 @@
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+    selector: 'mean-app',
+    template: '<router-outlet></router-outlet>',
+})
+export class AppComponent { }
